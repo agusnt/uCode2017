bd755ba9d42267d3cb9b7f84f37a1ad40ee8298a
Alex <asolanas93@gmail.com>
2017-03-11 16:25:23 +0100

    terrestre con imagen

--- terrestre.js
+++ terrestre.js
@@ -1,6 +1,10 @@
 var Cylon = require("cylon");
 var sumo = require('node-sumo-client');
 var cv = require('opencv');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
 
 var LEFT_TRESHOLD = 0.25,
     RIGHT_TRESHOLD = -0.25,
@@ -12,6 +16,9 @@ var video = drone.getVideoStream();
 var buf = null;
 var w = new cv.NamedWindow("Video", 0);
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index2.html');
+});
 
 drone.connect(function() {
     stream = drone.getVideoStream();
@@ -85,25 +92,38 @@ Cylon.robot({
   }
 }).start();
 
-setInterval(function () {
-    if (buf == null) {
-        return;
-    }
+io.on('connection', function (socket) {
+    console.log("Conexion");
+    setInterval(function () {
+        if (buf == null) {
+            return;
+        }
 
-    try {
-        cv.readImage(buf, function (err, im) {
-            if (err) {
-                console.log(err);
-            } else {
-                if (im.width() < 1 || im.height() < 1) {
-                    console.log("no width or height");
-                    return;
+        try {
+            cv.readImage(buf, function (err, im) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (im.width() < 1 || im.height() < 1) {
+                        console.log("no width or height");
+                        return;
+                    }
+                    w.show(im);
+                    im.save(__dirname +"/tmp/image.jpeg");
+                    fs.readFile(__dirname + '/tmp/image.jpeg', function(err, buf){
+                        // it's possible to embed binary data
+                        // within arbitrarily-complex objects
+                        socket.emit('data',  { image: true, buffer: buf.toString('base64') });
+                    });
+                    w.blockingWaitKey(0, 50);
                 }
-                w.show(im);
-                w.blockingWaitKey(0, 50);
-            }
-        });
-    } catch (e) {
-        console.log(e);
-    }
-}, 100);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }, 100);
+});
+
+http.listen(3000, function(){
+    console.log('Listening on *:3000')
+});
