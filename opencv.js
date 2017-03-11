7d8a4f30ab935aacfcf12dfe8acb77a6d308d880
Alex <asolanas93@gmail.com>
2017-03-11 11:49:51 +0100

    open cv done

--- opencv.js
+++ opencv.js
@@ -7,7 +7,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-var socket;
 
 var drone = sumo.createClient();
 var video = drone.getVideoStream();
@@ -27,39 +26,39 @@ video.on("data", function (data) {
     buf = data;
 });
 
-io.on('connection', function (sock) {
-    socket = sock
-    console.log("Conexion")
+io.on('connection', function (socket) {
+    console.log("Conexion");
+    setInterval(function () {
+        if (buf == null) {
+            return;
+        }
+
+        try {
+            cv.readImage(buf, function (err, im) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (im.width() < 1 || im.height() < 1) {
+                        console.log("no width or height");
+                        return;
+                    }
+                    w.show(im);
+                    im.save(__dirname +"/tmp/image.jpeg");
+                    fs.readFile(__dirname + '/tmp/image.jpeg', function(err, buf){
+                        // it's possible to embed binary data
+                        // within arbitrarily-complex objects
+                        socket.emit('data',  { image: true, buffer: buf.toString('base64') });
+                    });
+                    w.blockingWaitKey(0, 50);
+                }
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }, 100);
 });
 
-setInterval(function () {
-    if (buf == null) {
-        return;
-    }
 
-    try {
-        cv.readImage(buf, function (err, im) {
-            if (err) {
-                console.log(err);
-            } else {
-                if (im.width() < 1 || im.height() < 1) {
-                    console.log("no width or height");
-                    return;
-                }
-                w.show(im);
-                im.save(__dirname +"/tmp/image.jpeg");
-                fs.readFile(__dirname + '/tmp/image.jpeg', function(err, buf){
-                    // it's possible to embed binary data
-                    // within arbitrarily-complex objects
-                    socket.emit('data',  { image: true, buffer: buf.toString('base64') });
-                });
-                w.blockingWaitKey(0, 50);
-            }
-        });
-    } catch (e) {
-        console.log(e);
-    }
-}, 100);
 
 http.listen(3000, function(){
     console.log('Listening on *:3000')
