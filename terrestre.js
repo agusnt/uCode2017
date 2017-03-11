bd0928db8e5313578117e6e1aaab770e96b2c179
Agustin <agusnavarro11@gmail.com>
2017-03-11 02:32:21 +0100

    Drone and leapmotion

--- terrestre.js
+++ terrestre.js
@@ -1,12 +1,64 @@
+var Cylon = require("cylon");
 var sumo = require('node-sumo-client');
+
+var LEFT_TRESHOLD = 0.2,
+    RIGHT_TRESHOLD = -0.2,
+    UP_TRESHOLD = -0.2,
+    BACK_TRESHOLD = 0.2;
 
 var drone = sumo.createClient();
 
-drone.connect(function() {
-    drone.postureJumper();
-    drone.forward(50);
-
-    setTimeout(function() {
-        drone.right(50);
-    }, 1000);
+drone.connect(function()
+{
+    console.log("Drone OK")
 });
+
+Cylon.robot({
+  connections: {
+    leapmotion: { adaptor: 'leapmotion' }
+  },
+
+  devices: {
+    leapmotion: { driver: 'leapmotion' }
+  },
+
+  work: function(my) {
+    my.leapmotion.on('hand', function(payload) {
+        var handOpen = !!payload.fingers.filter(function(f) {
+                return f.extended;
+        }).length
+        if (handOpen)
+        {
+            if (payload.direction[1] < UP_TRESHOLD)
+            {
+                drone.forward((payload.direction[1] * -100));
+                console.log("Up");
+            } else if (payload.direction[1] > BACK_TRESHOLD)
+            {
+                drone.backward((payload.direction[1] * 100));
+                console.log("Back");
+            } else
+            {
+                drone.stop();
+                console.log("Stop");
+            }
+
+            if (payload.palmNormal[0] > LEFT_TRESHOLD)
+            {
+                drone.left((payload.palmNormal[0] * 100));
+                console.log("Left");
+            } else if (payload.palmNormal[0] < RIGHT_TRESHOLD)
+            {
+                drone.right((payload.palmNormal[0] * -100));
+                console.log("Right");
+            }
+        } else
+        {
+            drone.animationsLongJump();
+            //drone.animationsSpiral();
+            console.log("Jump");
+        }
+        //console.log(payload.toString());
+    });
+  }
+}).start();
